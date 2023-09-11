3369becb6d9fce846ce03171211c38ced708d7aa
等陌上花开 <zhzhn2002@126.com>
2023-09-11 09:28:02 +0800

    修复了一些已知问题

--- app/src/main/java/cloud/computer/app/MainActivity.java
+++ app/src/main/java/cloud/computer/app/MainActivity.java
@@ -2,23 +2,15 @@ package cloud.computer.app;
 
 import android.Manifest;
 import android.app.Activity;
-import android.content.Context;
-import android.os.Build;
 import android.os.Bundle;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.annotation.RequiresApi;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-
-import java.util.Arrays;
 
 
 public class MainActivity extends Activity {
     public static String[] ALL_PERMISSIONS = {
-            Manifest.permission.INTERNET,
-            Manifest.permission.ACCESS_NETWORK_STATE,
+            Manifest.permission.INTERNET,  // 网络权限
+            Manifest.permission.ACCESS_NETWORK_STATE, // 获取网络状态的权限
 
     };
     @Override
